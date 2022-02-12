6f43672df840842888e89573d9eeeef0ff845623
Thanuja <99541354+it21096570@users.noreply.github.com>
2022-02-12 16:20:51 +0000

    Tutorial Submission

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,22 @@
 #include <stdio.h>
 
 int main() {
+
+  int a, b;
+  float total, avg;
+
+  printf("Input mark 1 : ");
+  scanf("%d", &a);
+
+  printf("Input mark 2 : ");
+  scanf("%d", &b);
+
+  total = a + b;
+
+  avg = total / 2;
+
+  printf("Total = %.2f", avg);
+
   
   return 0;
 }
